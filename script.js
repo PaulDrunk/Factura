d1940779d0e4a53d1e4e5845a07123c0e2f26ffa
PavelBetiu <betiupavel@gmail.com>
2021-04-11 13:05:34 +0300

    Table insertion and deletion -> Done

--- script.js
+++ script.js
@@ -19,33 +19,8 @@ function myFunction() {
     cell6.innerHTML = "NEW CELL6";
     cell7.innerHTML = "NEW CELL7";
     cell8.innerHTML = "NEW CELL8";
-    cell3.style.borderLeft = "1px white";
-    cell2.style.borderRight = "1px white";
 }
-
-const inpdenproduse = document.getElementById("denprodus");
-const inpum = document.getElementById("um");
-const inpcantitate = document.getElementById("cantitate");
-const inppretunitarfaratva = document.getElementById("pretunitfaratva");
-const insertbtn = document.getElementById("btninsert");
-insertbtn.onclick = function (){
-    const denproduse = inpdenproduse.value;
-    const um = inpum.value;
-    const cantitate = inpcantitate.value;
-    const pretunitarfaratva = inppretunitarfaratva.value;
-
-    console.log(denproduse);
-    console.log(um);
-    console.log(cantitate);
-    console.log(pretunitarfaratva);
-
-    if(denproduse && um && cantitate && pretunitarfaratva){
-        localStorage.setItem(denproduse, um, cantitate, pretunitarfaratva);
-        location.reload();
-    }
-}
-function insertFunction() {
-    var counter = 0;
+function localstorage(){
     const inpdenproduse = document.getElementById("denprodus");
     const inpum = document.getElementById("um");
     const inpcantitate = document.getElementById("cantitate");
@@ -68,8 +43,46 @@ function insertFunction() {
         }
     }
 }
+function insertFunction() {
+    counter++;
+    var table = document.getElementById("InvoiceTable");
+    var row = table.insertRow(counter + 2);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7);
+    cell3.style.borderLeft = "1px white";
+    cell2.style.borderRight = "1px white";
+    cell1.innerHTML = counter + 1;
+
+    var inpdenproduse = document.getElementById("denprodus").value;
+    cell2.innerHTML = inpdenproduse;
+
+    cell3.innerHTML = "";
+    var inpum = document.getElementById("um").value;
+    cell4.innerHTML = inpum;
+
+    var inpcantitate = document.getElementById("cantitate").value;
+    cell5.innerHTML = inpcantitate;
+
+    var inppretunitarfaratva = document.getElementById("pretunitfaratva").value;
+    cell6.innerHTML = inppretunitarfaratva;
+
+    cell7.innerHTML = inpcantitate * inppretunitarfaratva;
+
+    var valoaretvalei =  0.1 * inpcantitate * inppretunitarfaratva;
+    cell8.innerHTML = valoaretvalei;
+}
 function eraseRow()
 {
+    if(counter == 0){
+        return;
+    }
+    counter--;
     var table = document.getElementById("InvoiceTable");
     var rowCount = table.rows.length;
     table.deleteRow(rowCount - 14);
